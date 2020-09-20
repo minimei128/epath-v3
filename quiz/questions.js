a18e82c4e76fa31507023ab5d490c4700f6c948a
minimei128 <mphan2868@hotmail.com8>
2020-09-21 02:39:55 +1000

    update quiz layout

---
+++ quiz/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "Q1. Primarily, inside which tag of an HTML document do you put the JavaScript?",
+        choices: ["<java>", "<body>", "<script>", "<img>"],
+        answer: "<script>"
+    },
+    {
+        title: "Q2. The condition in an if/else statement is enclosed within ______.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Q3. What is the syntax for referencing an external script called 'abc.js'?",
+        choices: ["<script class='abc.js'>", "<script href='abc.js'>", "<script link='abc.js'>", "<script src='abc.js'>"],
+        answer: "<script src='abc.js'>"
+    },
+    {
+        title: "Q4. Which type of pop up box will allow a user to type a response?",
+        choices: ["input", "prompt", "alert", "confirm"],
+        answer: "prompt"
+    },
+    {
+        title: "Q5. What is a DOM in JavaScript?",
+        choices: ["Data of Mine", "Document of Master", "Data Object Modal", "Document Object Model"],
+        answer: "Document Object Model"
+    },
+    {
+        title: "Q6. Is JS case-sensitive?",
+        choices: ["Yes", "No", "I have no clue", "Only when it feels like it."],
+        answer: "Yes"
+    },
+];
